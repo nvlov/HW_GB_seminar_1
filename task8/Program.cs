22cc06dbd68e8abb22adb80347f336e1369522b6
Nikolay <nlovtsevich@gmail.com>
2023-06-12 18:02:42 +0300

    task8  done

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, 
+// которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Even numbers from 1 to {0}:", N);
+        
+        for (int i = 2; i <= N; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
